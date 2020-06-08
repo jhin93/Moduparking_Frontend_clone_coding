c2e9d06e505d7df62b8dd195ab48407b53f46acb
henry <henry@moduparking.com>
2020-06-09 08:52:53 +0900

    서체 기본 스타일 추가, HelloWorld 컴포넌트에 스타일 적용 예시 추가

--- src/App.vue
+++ src/App.vue
@@ -8,13 +8,14 @@
   </div>
 </template>
 
+<!--todo: Notosans KR 로 수정-->
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #3f3f3f;
 }
 
 #nav {
