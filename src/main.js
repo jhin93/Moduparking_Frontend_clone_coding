3457e80c86b04e1e97a72e5ada98b391c3860dbe
zpxlffjrm <scm0222@naver.com>
2020-06-09 15:45:41 +0900

    updated the_navigator.vue, package*, main.js. navigation updated and vuetify installed.

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './style/typhography.css'
+import vuetify from './plugins/vuetify'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
