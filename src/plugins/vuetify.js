aafa017f634d480fc6074d447d22ac3570845467
zpxlffjrm <scm0222@naver.com>
2020-06-10 13:07:35 +0900

    updated src/plugins/vuetify.js, updated src/style/typhograph.css

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
-
+import colors from 'vuetify/es5/util/colors'
 Vue.use(Vuetify)
 
 export default new Vuetify({
-  icons: {
-    iconfont: 'md'
+  theme: {
+    primary: '#0099ff',
+    secondary: colors.grey.darken1,
+    accent: colors.shades.black,
+    error: colors.red.accent3
   }
 })
