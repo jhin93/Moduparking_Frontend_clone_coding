97879a98899eec27803767c4849adbd82d12eea2
zpxlffjrm <scm0222@naver.com>
2020-06-16 15:27:41 +0900

    update views, style, router

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import HowToUse from '../views/howToUse.vue'
+import news from '../views/news.vue'
+import article20200113 from '../views/20200113-article'
+import article20200604 from '../views/20200604-article'
+import QnA from '../views/QnA'
 // 5번줄 import->require로 수정해서 에러 해결
 const Terms = () => require(/* webpackChunkName: "terms" */ '../views/terms.vue')
 
@@ -19,9 +23,29 @@ const routes = [
     component: Terms
   },
   {
-    path: '/HowToUse',
+    path: '/elementor-345',
     name: 'howToUse',
     component: HowToUse
+  },
+  {
+    path: '/elementor-624',
+    name: 'news',
+    component: news
+  },
+  {
+    path: '/2020/06/04/elementor-645',
+    name: 'news',
+    component: article20200604
+  },
+  {
+    path: '/2020/01/13/a-simple-guide-to-design-thinking/',
+    name: 'news',
+    component: article20200113
+  },
+  {
+    path: '/300f0-portfolio',
+    name: 'QnA',
+    component: QnA
   }
 ]
 
