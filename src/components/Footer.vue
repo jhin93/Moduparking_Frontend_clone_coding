6731c550a4753c41d24f6539f4d5048a5affa5b4
jhin93 <scene1993@gmail.com>
2020-06-26 12:12:36 +0900

    푸터 메일링크 완료(누락 추가)

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,8 +2,10 @@
   <v-footer class="footer-content">
     <v-flex>
       <div class="footer__text">
+        <form>
         상호명 : 주식회사 모두컴퍼니 | 사업자등록번호 : 220-88-52570 | 통신판매업 신고번호 : 제2017-서울중구-0923호<br>
-        공동대표 : 강수남, 김동현 | 주소 : 서울시 성동구 왕십리로 115, 헤이그라운드 8층 (우) 04768 | 전화 : 1899-8242 | contact@moduparking.com | &copy; {{ new Date().getFullYear() }} 쉽고 편리한 주차 플랫폼 모두의주차장 All Right Reserved
+        공동대표 : 강수남, 김동현 | 주소 : 서울시 성동구 왕십리로 115, 헤이그라운드 8층 (우) 04768 | 전화 : 1899-8242 <v-btn depressed flat class="px-0 q-v-btn" @click="send" type="submit">contact@moduparking.com</v-btn> | &copy; {{ new Date().getFullYear() }} 쉽고 편리한 주차 플랫폼 모두의주차장 All Right Reserved
+        </form>
       </div>
     </v-flex>
   </v-footer>
@@ -11,7 +13,12 @@
 
 <script>
 export default {
-  name: 'footer'
+  name: 'footer',
+  methods: {
+    send () {
+      window.open('mailto:contact@moducompany.com')
+    }
+  }
 }
 </script>
 
